8fc7908ae595bf0097345459fa9db8a806911a8a
gabriele <gabriele.fugagnoli@gmail.com>
2023-08-08 17:28:34 +0200

    Piccoli aggiustamenti, comincio ad implementare il codice per trovare serie stagionali

--- scripts/feature_analysis.py
+++ scripts/feature_analysis.py
@@ -10,23 +10,23 @@ import copy
 
 #ritorna un dizionario con i valori di trend e seasonality associati agli id del prodotto
 def stl(dic, period = 7):
-    """for p in dic:
-      dic[p].reset_index(inplace = True)
-      dic[p].columns = ['ds', 'y']
+    # for p in dic:
+    #   dic[p].reset_index(inplace = True)
+    #   dic[p].columns = ['ds', 'y']
 
-    pred = dict()
-    m = Prophet()
+    # pred = dict()
+    # m = Prophet()
 
-    for p in dic.keys():
+    # for p in dic.keys():
       
-      m.fit(dic[p])
-      future = m.make_future_dataframe(periods=30)
-      pred[p] = m.predict(future)
+    #   m.fit(dic[p])
+    #   future = m.make_future_dataframe(periods=30)
+    #   pred[p] = m.predict(future)
 
-    with open(processed_dir / 'pred.pkl', 'wb') as f:
-      pickle.dump(pred, f)
+    # with open(processed_dir / 'pred.pkl', 'wb') as f:
+    #   pickle.dump(pred, f)
 
-    return pred """
+    # return pred 
     decompose = seasonal_decompose(dic[55]['Amount_sold'],model='additive', period=period)
     decompose.plot()
     plt.show()
@@ -53,19 +53,19 @@ if __name__ == "__main__":
     plt.show()
     #stl_par = stl(dic)
     
-    """dic[55].reset_index(inplace = True)
-    dic[55].columns = ['ds', 'y']
-    print(dic[55].tail())
+    # dic[55].reset_index(inplace = True)
+    # dic[55].columns = ['ds', 'y']
+    # print(dic[55].tail())
 
-    pred = dict()
-    m = Prophet()
+    # pred = dict()
+    # m = Prophet()
 
       
-    m.fit(dic[55])
-    future = m.make_future_dataframe(periods=30)
-    pred[55] = (m.predict(future).iloc[:len(future) - 30]).loc[:,['trend', 'weekly', 'yearly']]
-    plt.plot(pred[55])
-    plt.show()
-    """
+    # m.fit(dic[55])
+    # future = m.make_future_dataframe(periods=30)
+    # pred[55] = (m.predict(future).iloc[:len(future) - 30]).loc[:,['trend', 'weekly', 'yearly']]
+    # plt.plot(pred[55])
+    # plt.show()
+    
 
     #print(pred[55].columns.tolist())
